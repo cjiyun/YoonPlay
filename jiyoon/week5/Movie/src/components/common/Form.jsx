70c1bdda89de76424ed8440a63cffa76dcb090e8
cjiyun <chlwldbs78@gmail.com>
2025-05-29 11:55:29 +0900

    유효성 검사 훅 분리

--- jiyoon/week5/Movie/src/components/common/Form.jsx
+++ jiyoon/week5/Movie/src/components/common/Form.jsx
@@ -1,31 +1,8 @@
-import * as yup from "yup";
-import {useForm} from "react-hook-form";
-import {yupResolver} from "@hookform/resolvers/yup";
 import Input from "./Input";
+import useValidate from "../../hooks/useValidate";
 
 const Form = ({ path }) => {
-  const schema = yup.object().shape({
-    email: yup.string()
-      .required('이메일을 입력해주세요.')
-      .matches(/^[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,}$/, '올바른 이메일 형식을 입력해주세요.'),
-    password: yup.string()
-      .required('비밀번호를 입력해주세요.')
-      .min(8, '비밀번호는 8자 이상이어야 합니다.')
-      .max(16, '비밀번호는 16자 이하여야 합니다.'),
-    ...(path === "/signup" && {
-      passwordCheck: yup.string()
-        .oneOf([yup.ref('password'), null], '비밀번호가 일치하지 않습니다')
-        .required('비밀번호를 한 번 더 입력해주세요.'),
-    }),
-  })
-  const {
-    register,
-    handleSubmit,
-    formState: {errors, touchedFields, isValid}
-  } = useForm({
-    mode: 'onChange',
-    resolver: yupResolver(schema),
-  });
+  const { handleSubmit, isValid, ...res } = useValidate({path})
   const onSubmit = (data) => {
     console.log('submit');
     console.log(data);
@@ -42,9 +19,7 @@ const Form = ({ path }) => {
             name={name}
             type={name === 'email' ? 'text' : name}
             placeholder={name === "email" ? "이메일을 입력해주세요" : "비밀번호를 입력해주세요"}
-            register={register}
-            touchedFields={touchedFields}
-            errors={errors}
+            {...res}
           />
         ))}
         {path === "/signup" &&
@@ -52,9 +27,7 @@ const Form = ({ path }) => {
             name="passwordCheck"
             type="password"
             placeholder="비밀번호를 다시 입력해주세요"
-            register={register}
-            touchedFields={touchedFields}
-            errors={errors}
+            {...res}
           />
         }
       </>
